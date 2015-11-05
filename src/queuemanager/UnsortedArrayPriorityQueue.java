bed1ef698b1092fb7df2098dc1deeb58f391ed71
09004316 <09004316@MC-C-N4712.uhi.ad.local>
2015-11-05 17:18:25 +0000

    Restarted UnsortedArray Implementation - Add Function: Almost works except array is displaying the wrong way round (Maybe adding to the wrong side of the array?)

--- src/queuemanager/UnsortedArrayPriorityQueue.java
+++ src/queuemanager/UnsortedArrayPriorityQueue.java
@@ -1,22 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package queuemanager;
 
-/**
- *
- * @author 09004316
- */
+
 public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
-    
+
     private final Object[] storage;
-    
+
+    /**
+     * The size of the storage array.
+     */
     private final int capacity;
-    
+
+    /**
+     * The index of the last item stored.
+     *
+     * This is equal to the item count minus one.
+     */
     private int tailIndex;
 
+    /**
+     * Create a new empty queue of the given size.
+     *
+     * @param size
+     */
     public UnsortedArrayPriorityQueue(int size) {
         storage = new Object[size];
         capacity = size;
@@ -24,45 +29,58 @@ public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
     }
 
     @Override
-    public void add(T item, int priority) throws QueueOverflowException {
-        if(tailIndex == capacity - 1)
-        {
-            throw new UnsupportedOperationException();
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return ((Wrapper<T>) storage[0]).getItem();
         }
-        else
-        {
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        if (tailIndex == capacity - 1) {
+            /* No resizing implemented, but that would be a good enhancement. */
+            throw new QueueOverflowException();
+        } else {
             tailIndex = tailIndex + 1;
             int i = tailIndex;
-            while (i > 0 && ((Wrapper<T>) storage[i - 1]).getPriority() == item)
-            {
+            while (i > 0 && ((Wrapper<T>) storage[i - 1]).getPriority() == priority) {
                 storage[i] = storage[i - 1];
                 i = i - 1;
+                tailIndex = priority;
             }
             storage[i] = new Wrapper<>(item, priority);
         }
     }
 
     @Override
-    public T head() throws QueueUnderflowException {
-        if(isEmpty())
-        {
-            throw new UnsupportedOperationException();
-        }
-        else
-        {
-            return ((Wrapper<T>) storage[0]).getItem();
-        }
-        
-    }
-
-    @Override
     public void remove() throws QueueUnderflowException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            for (int i = 0; i < tailIndex; i++) {
+                storage[i] = storage[i + 1];
+            }
+            tailIndex = tailIndex - 1;
+        }
     }
 
     @Override
     public boolean isEmpty() {
         return tailIndex <= -1;
     }
-    
+
+    @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i <= tailIndex; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + storage[i];
+        }
+        result = result + "]";
+        return result;
+    }
 }
