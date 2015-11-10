433a57b8a3071cb2fb39395a2e3e451aeaa70563
09004316 <09004316@MC-C-N4712.uhi.ad.local>
2015-11-10 16:41:28 +0000

    Created Node.java Began SortedLinkedPriorityQueue Completed head() function of UnsortedArrayPriorityQueue

---
+++ src/queuemanager/Node.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+public class Node<T> {
+    private T item;
+    private Node<T> next;
+    private int priority;
+    
+    public Node(T item, Node<T> next, int priority) {
+        this.item = item;
+        this.next = next;
+        this.priority = priority;
+    }
+    
+    public T getItem() {
+        return item;
+    }
+    
+    public Node<T> getNext() {
+        return next;
+    }
+    
+    public void setNext(Node<T> node) {
+        this.next = node;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+     
+}
+
